7c85e06414430f1b27e4af00b064694e2789c3a3
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2020-01-13 03:52:51 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,18 +11,12 @@ require "pry"
 
  
 def create_project_hash
-    projects = {} 
-  html = File.read('fixtures/kickstarter.html')
-  kickstarter = Nokogiri::HTML(html)
-  
   projects = {} 
-  
   kickstarter.css("li.project.grid_4").each do |project|
-    projects[project] = {}
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {}
   end
  
   # return the projects hash
   projects
 end
-end
- 
